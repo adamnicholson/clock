e2afeac4ae3d515d507aad268edf688a15cbeb41
Adam Nicholson <adamnicholson10@gmail.com>
2025-05-27 11:23:46 +0100

    Ad psr/clock compatability

--- src/Clock.php
+++ src/Clock.php
@@ -2,12 +2,17 @@
 
 namespace Adamnicholson\Clock;
 
-interface Clock
+interface Clock extends \Psr\Clock\ClockInterface
 {
+    /**
+     * @return \DateTimeImmutable
+     */
     public function getCurrentDatetime(): \DateTimeImmutable;
 
     /**
      * Alias of getCurrentDatetime()
+     *
+     * @return \DateTimeImmutable
      */
     public function now(): \DateTimeImmutable;
 }
